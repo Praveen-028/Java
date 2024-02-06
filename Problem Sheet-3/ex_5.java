2c7051e64dcfbdc459e59e11032b1a5519bfb696
Praveen Y <142769042+Praveen-028@users.noreply.github.com>
2024-02-06 09:54:52 +0000

    Ps-3 final

--- Problem Sheet-3/ex_5.java
+++ Problem Sheet-3/ex_5.java
@@ -1,3 +1,11 @@
 public class ex_5 {
+    public static void main(String[] args) {
+        boolean a,b,Q;
+        a=true;
+        b=true;
+        Q= (!(a&&b)&&(a||b))||((a&&b)||!(a||b));
+
+        System.out.println("The final Output is :"+Q);
+    }
     
 }
