6b01d36e1b2e960a78dc6e2e6326b83a403eb076
Praveen Y <142769042+Praveen-028@users.noreply.github.com>
2024-02-16 06:31:20 +0000

    an

--- Problem Sheet-5/ex_2_5.java
+++ Problem Sheet-5/ex_2_5.java
@@ -1,5 +1,28 @@
+import java.util.*;
 public class ex_2_5 {
     public static void main(String[] args) {
-        
+
+        Scanner sc=new Scanner(System.in);
+        int inputNumber=sc.nextInt();
+        boolean isItPrime=true;
+        for (int j=0;j)
+
+        for (int i = 2; i<= inputNumber/2; i++)
+        {
+        if ((inputNumber % i) == 0)
+        {
+        isItPrime = false;
+        break;
+        }
+        }
+        if (isItPrime)
+        {
+            System.out.println(inputNumber+"is a prime number");
+        }
+        else
+        {
+            System.out.println("Not a prime number");
+        }
+        sc.close();
     }
 }
---
+++ oop-java/enum_1.java
@@ -0,0 +1,8 @@
+class enum_1 {
+    public enum Season {WINTER,Spring,SUMMER,Fall}
+    public static void main (String[]args)
+    {
+        for (Season s:Season.values())
+        {System.out.println(s);}
+    }
+}
