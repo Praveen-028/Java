7f4ad60117b8700162f3a301ebdd7aa488efb724
Praveen Y <142769042+Praveen-028@users.noreply.github.com>
2024-02-20 08:08:03 +0000

    tyggu

--- Problem Sheet-4/ex_3_4.java
+++ Problem Sheet-4/ex_3_4.java
@@ -7,15 +7,13 @@ public class ex_3_4
         System.out.print("Enter the Number :");
         int rows=sc.nextInt();
         for (int i = 1; i <= rows; i++) {
-            // Print spaces
             for (int j = 1; j <= rows - i; j++) {
                 System.out.print(" ");
             }
-            // Print decreasing sequence
+
             for (int j = i; j >= 1; j--) {
                 System.out.print(j);
             }
-            // Print increasing sequence
             for (int j = 2; j <= i; j++) {
                 System.out.print(j);
             }
