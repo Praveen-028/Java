c823643859f4ad847063f2d175cb259f7b8a2db8
Praveen Y <142769042+Praveen-028@users.noreply.github.com>
2024-02-23 06:50:28 +0000

    prob6

---
+++ Problem Sheet-6/ex_3_6.java
@@ -0,0 +1,12 @@
+public class ex_3_6 {
+    
+}
+class CoffeeBag{
+    float priceperpound=150.99f;
+    float taxrate=0.0725f;
+    
+    public double totalPrice(double )
+    {
+        
+    }
+}
