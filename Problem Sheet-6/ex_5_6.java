127090d91e714dc3a521bfb70b467c676ae5545d
Praveen-028 <praveenyu02@gmail.com>
2024-03-05 15:09:37 +0530

    comm

--- Problem Sheet-6/ex_5_6.java
+++ Problem Sheet-6/ex_5_6.java
@@ -10,17 +10,17 @@ class Queue{
     }
     public void enqueue(int v)
     {
-        if (rear<this.size)
+        if (rear<this.size-1)
         {
             this.array[rear]=v;
-            this.rear=rear++;
+            this.rear++;
         }
         else
         {
             int arr2[]=new int[size*2];
             for(int i=0;i<rear;i++)
             {
-                array[i]=arr2[i];
+                arr2[i]=this.array[i];
             }
             this.array=arr2;
             this.size=size*2;
@@ -31,7 +31,7 @@ class Queue{
     public int dequeue()
     {
         int returne=array[0];
-        for(int i=0;i<=rear;i++)
+        for(int i=0;i<rear;i++)
         {
             array[i]=array[i+1];
             array[i+1]=0;
@@ -41,14 +41,37 @@ class Queue{
     }
     public int empty()
     {
-        if(this.rear>=-1)
+        if(this.rear<=-1)
         {
             return 0;
         }
-        return 0;
+        return 1;
     }
     public int getSize()
     {
         return this.rear ;
     }
 }
+    public class ex_5_6{
+        public static void main(String[]args)
+        {
+            Queue q =new Queue();
+            for(int i=1;i<20;i++)
+            {
+                q.enqueue(i);
+            }
+            System.out.println("The Size of the queue is : "+q.getSize());
+            System.out.println("\nelements :");
+            for(int i=1;i<20;i++)
+            {
+                System.out.print(q.dequeue()+" ");
+            }
+            System.out.println(" ");
+            System.out.println("The Size of the queue is : "+q.getSize());
+            System.out.println("\nAfter deletion:\n"); 
+            for(int i=1;i<20;i++)
+            {
+                System.out.print(q.dequeue()+" ");
+            }
+        }
+    }
--- Problem Sheet-7/ex_1_7.java
+++ Problem Sheet-7/ex_1_7.java
@@ -1,5 +1,56 @@
+
 class account{
-    
+    private double bal;
+    private int accnum;
+    public account(int a){
+        bal=0.0;
+        accnum=a;
+    }
+    public void deposit(double sum)
+    {
+        if(sum>0)
+        {
+            bal+=sum;
+        }
+        else
+        {
+            System.out.println("Deposit amount cannot ne negative");
+        }
+    }
+    public void withdraw(double sum)
+    {
+        if(sum>0)
+        {
+            bal-=sum;
+        }
+        else{
+            System.out.println("The withdraw amount can't be negaitve");
+        }
+    }
+    public double getBalance()
+    {
+        return bal;
+    }
+    public double getAccountNumber()
+    {
+        return accnum;
+    }
+    public String toString()
+    {
+        return ("Acc"+accnum+"; Balance ="+bal);
+    }
+    public final void print()
+    {
+        System.out.println(toString());
+    }
+}
+class Savingsaccount extends account
+{
+    private double interest;
+    public Savingsaccount(){
+        
+    }
+
 }
 
 public class ex_1_7 {
