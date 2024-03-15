d43f0d599f3db021c9ebe50b2d23670328253c55
Praveen <praveenyu02@gmail.com>
2024-03-15 07:44:55 +0530

    ex1_7

---
+++ Problem Sheet-7/ex_2_7.java
@@ -0,0 +1,11 @@
+class Employee{
+    private String Emp_name;
+    private int Emp_id;
+    private String Address;
+    private String Mail_id;
+    private int Phone_no;
+}
+class
+public class ex_2_7 {
+    
+}
