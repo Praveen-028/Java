dc10ce1597a59f948f066fc1d6d65bd1f005b804
Praveen-028 <praveenyu02@gmail.com>
2024-03-15 10:52:00 +0530

    ex_2_7

---
+++ Problem Sheet-7/ex_2_7.java
@@ -0,0 +1,18 @@
+class Employee{
+    private String Emp_name;
+    private int Emp_id;
+    private String Address;
+    private String Mail_id;
+    private int Mobile_no;
+
+    public Employee(String name,int id,String Addres,String Mail,int Mobile){
+        this.Emp_name=name;
+        this.Emp_id=id;
+        this.Address=Addres;
+        this.Mail_id=Mail;
+        this.Mobile_no=Mobile;
+    }
+}
+public class ex_2_7 {
+    
+}
