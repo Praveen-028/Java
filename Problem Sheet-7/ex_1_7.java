e64b1492f7258af8ebf0a604abf202a4be4715ad
Praveen-028 <praveenyu02@gmail.com>
2024-03-15 10:41:29 +0530

    1_7

--- Problem Sheet-7/ex_1_7.java
+++ Problem Sheet-7/ex_1_7.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 class account{
     private double bal;
@@ -44,15 +45,103 @@ class account{
         System.out.println(toString());
     }
 }
-class Savingsaccount extends account
-{
-    private double interest;
-    public Savingsaccount(){
-        
+class SavingsAccount extends account {
+    private double interestRate;
+
+    public SavingsAccount(int accnum, double interestRate) {
+        super(accnum);
+        this.interestRate = interestRate;
     }
 
+    public void addInterest() {
+        double interest = getBalance() * interestRate / 100;
+        deposit(interest);
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + "; Interest Rate =" + interestRate;
+    }
 }
 
-public class ex_1_7 {
+class CurrentAccount extends account {
+    private double overdraftLimit;
+
+    public CurrentAccount(int accnum, double overdraftLimit) {
+        super(accnum);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (getBalance() - amount >= -overdraftLimit) {
+            super.withdraw(amount);
+        } else {
+            System.out.println("Exceeds overdraft limit!");
+        }
+    }
     
+    @Override
+    public String toString() {
+        return super.toString() + "; Overdraft Limit =" + overdraftLimit;
+    }
+}
+class Bank {
+    private ArrayList<account> accounts;
+
+    public Bank() {
+        accounts = new ArrayList<>();
+    }
+
+    public void openAccount(account account) {
+        accounts.add(account);
+    }
+
+    public void closeAccount(account account) {
+        accounts.remove(account);
+    }
+
+    public void payDividend(double amount) {
+        for (account account : accounts) {
+            account.deposit(amount);
+        }
+    }
+
+    public void update() {
+        for (account account : accounts) {
+            if (account instanceof SavingsAccount) {
+                ((SavingsAccount) account).addInterest();
+            } else if (account instanceof CurrentAccount) {
+                CurrentAccount currentAccount = (CurrentAccount) account;
+                if (currentAccount.getBalance() < 0) {
+                    System.out.println("Sending letter to account " + currentAccount.getAccountNumber() + " for overdraft.");
+                }
+            }
+        }
+    }
+}
+public class ex_1_7 {
+    public static void main(String[] args) {
+        Bank bank = new Bank();
+
+        // Test accounts
+        SavingsAccount savings = new SavingsAccount(123, 2.5);
+        bank.openAccount(savings);
+        savings.deposit(1000);
+
+        CurrentAccount current = new CurrentAccount(456, 500);
+        bank.openAccount(current);
+        current.deposit(1000);
+        current.withdraw(1500);
+
+        // Update bank
+        bank.update();
+
+        // Pay dividend
+        bank.payDividend(50);
+
+        // Close accounts
+        bank.closeAccount(savings);
+        bank.closeAccount(current);
+}
 }
