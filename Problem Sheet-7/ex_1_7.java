aedb32a11fab23a530cc96b6cb60acf8bd437e97
Praveen <praveenyu02@gmail.com>
2024-04-25 18:13:18 +0530

    test

--- Problem Sheet-7/ex_1_7.java
+++ Problem Sheet-7/ex_1_7.java
@@ -3,10 +3,14 @@ import java.util.ArrayList;
 class account{
     private double bal;
     private int accnum;
+
+    //constructor
     public account(int a){
         bal=0.0;
         accnum=a;
     }
+
+    //deposit method
     public void deposit(double sum)
     {
         if(sum>0)
@@ -18,6 +22,7 @@ class account{
             System.out.println("Deposit amount cannot ne negative");
         }
     }
+    //withdrawal method
     public void withdraw(double sum)
     {
         if(sum>0)
@@ -28,10 +33,12 @@ class account{
             System.out.println("The withdraw amount can't be negaitve");
         }
     }
+    //returns balance
     public double getBalance()
     {
         return bal;
     }
+    //
     public double getAccountNumber()
     {
         return accnum;
@@ -58,7 +65,7 @@ class SavingsAccount extends account {
         deposit(interest);
     }
     
-    @Override
+    // @Override
     public String toString() {
         return super.toString() + "; Interest Rate =" + interestRate;
     }
@@ -121,6 +128,31 @@ class Bank {
     }
 }
 public class ex_1_7 {
-    
-}
+    public static void main(String[] args) {
+        // Experiment with instances of the Account class
+        account account1 = new account(1001);
+        account1.deposit(500);
+        account1.withdraw(200);
+        System.out.println("Account 1 balance: " + account1.getBalance());
+
+        // Experiment with instances of the SavingsAccount class
+        SavingsAccount savingsAccount1 = new SavingsAccount(2001, 5.0); // Interest rate of 5%
+        savingsAccount1.deposit(1000);
+        savingsAccount1.addInterest(); // Add interest
+        System.out.println("Savings Account 1 balance: " + savingsAccount1.getBalance());
+
+        // Experiment with instances of the CurrentAccount class
+        CurrentAccount currentAccount1 = new CurrentAccount(3001, 1000); // Overdraft limit of 1000
+        currentAccount1.deposit(800);
+        currentAccount1.withdraw(1200); // This will exceed the overdraft limit
+        System.out.println("Current Account 1 balance: " + currentAccount1.getBalance());
+
+        // Experiment with the Bank class
+        Bank bank = new Bank();
+        bank.openAccount(account1);
+        bank.openAccount(savingsAccount1);
+        bank.openAccount(currentAccount1);
+        bank.payDividend(50); // Pay dividend to all accounts
+        bank.update(); // Update accounts in the bank
+    }  
 }
